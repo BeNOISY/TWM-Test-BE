6dfb50c03c68cea940a2b5d98197fbe89310096b
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-04 17:32:21 +0200

    Entity Added

--- src/main/java/com/docker/compose/user/persistance/entity/User.java
+++ src/main/java/com/docker/compose/user/persistance/entity/User.java
@@ -1,17 +1,19 @@
 package com.docker.compose.user.persistance.entity;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@Document(collection = "UserDB")
+@Entity
 public class User {
 
 
-    @Id
+    @GeneratedValue
     private long _id;
 
     @NotBlank
