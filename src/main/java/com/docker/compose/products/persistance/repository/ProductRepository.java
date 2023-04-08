5cf8517b80098bc2836633c01a38f5f5acbb6e50
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-08 15:54:42 +0200

    AddRequest Test

--- src/main/java/com/docker/compose/products/persistance/repository/ProductRepository.java
+++ src/main/java/com/docker/compose/products/persistance/repository/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends MongoRepository <Product, Long > {
+public interface ProductRepository extends MongoRepository <Product, Long> {
     @Query("{ 'type' : ?0 }")
     List<Product> findProductByType(String type);
 
--- src/main/java/com/docker/compose/products/service/ProductServiceImpl.java
+++ src/main/java/com/docker/compose/products/service/ProductServiceImpl.java
@@ -36,9 +36,6 @@ public class ProductServiceImpl implements ProductService {
             productUpdate.setCount(product.getCount());
             productUpdate.setDescription(product.getDescription());
             productUpdate.setImg(product.getImg());
-
-            product.setByType(product.getType());
-
             productRepository.save(productUpdate);
             return productUpdate;
         } else {
@@ -54,13 +51,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List < Product > getProductByType(String type) {
-        List<Product> productDb = this.productRepository.findProductByType(type);
-        if (productDb.isEmpty()) {
-            throw new ResourceNotFoundException("Record not found with type : " + type);
-        } else {
-            return this.productRepository.findProductByType(type);
-        }
-
+        return this.productRepository.findProductByType(type);
     }
 
     @Override
