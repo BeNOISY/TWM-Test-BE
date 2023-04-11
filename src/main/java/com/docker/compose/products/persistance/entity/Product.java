298db9b23c111e2600a1eb930acfc8ebbd96eeb6
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-11 07:11:20 +0200

    constructor change

--- src/main/java/com/docker/compose/products/persistance/entity/Product.java
+++ src/main/java/com/docker/compose/products/persistance/entity/Product.java
@@ -6,71 +6,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "ProductDB")
 public class Product {
 
-    @Id
-    private long id;
-    private String name;
-    private String type;
-    private float price;
-    private long count;
-    private String description;
-    private String img;
-
-
-    //Basic Getters ------------------------------
-    public long getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getType() {
-        return type;
-    }
-    public float getPrice() {
-        return price;
-    }
-    public long getCount() {
-        return count;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public String getImg() {
-        return img;
-    }
-
-
-
-    //Setters --------------------------------------------
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-    public void setCount(long count) {
-        this.count = count;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    Specifics spec = new Specifics();
     public Product(){
-        switch (getType()){
+        switch (this.getType()){
             case "PSU":
                 spec.setModular(spec.isModular());
                 spec.setPlug(spec.getPlug());
@@ -91,6 +28,70 @@ public class Product {
                 break;
         }
     }
+
+    @Id
+    private long id;
+    private String name;
+    private String type;
+    private float price;
+    private long count;
+    private String description;
+
+    private String img;
+
+    //Basic Getters ------------------------------
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public long getCount() {
+        return count;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+
+    //Setters --------------------------------------------
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    Specifics spec = new Specifics();
 
 }
 
