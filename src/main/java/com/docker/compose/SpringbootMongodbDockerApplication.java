6879ee0debb71fbef8a869e6ec9985b396922ac5
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-12 17:46:01 +0200

    Delete function

--- src/main/java/com/docker/compose/SpringbootMongodbDockerApplication.java
+++ src/main/java/com/docker/compose/SpringbootMongodbDockerApplication.java
@@ -1,16 +1,7 @@
 package com.docker.compose;
 
-import com.docker.compose.user.persistance.entity.User;
-import com.docker.compose.user.persistance.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-
-import java.util.List;
 
 @SpringBootApplication
 public class SpringbootMongodbDockerApplication {
@@ -18,31 +9,5 @@ public class SpringbootMongodbDockerApplication {
 		SpringApplication.run(SpringbootMongodbDockerApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(UserRepository repository, MongoTemplate mongoTemplate){
-		return args -> {
-			String username = "Yondaime";
-			User user = new User(
-					username,
-					"Simon Baranec",
-					true,
-					"simonbaranec58@gmail.com",
-					"Prirodna 37",
-					"Nova Bana"
-			);
-
-		};
-	}
-
-	private void method(UserRepository repository, MongoTemplate mongoTemplate,String username,User user){
-		Query query = new Query();
-		query.addCriteria(Criteria.where("username").is(username));
-
-		List<User> users = mongoTemplate.find(query, User.class);
-
-		if(users.isEmpty()){
-			repository.insert(user);
-		}
-	}
 
 }
--- src/main/java/com/docker/compose/user/persistance/repository/UserRepository.java
+++ src/main/java/com/docker/compose/user/persistance/repository/UserRepository.java
@@ -4,8 +4,4 @@ package com.docker.compose.user.persistance.repository;
 import com.docker.compose.user.persistance.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository <User, String > {
-    Optional<User> getUserByUsername(String username);
-}
+public interface UserRepository extends MongoRepository <User, String > {}
