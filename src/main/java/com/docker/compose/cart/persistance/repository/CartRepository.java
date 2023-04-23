6b7d219bc9bb72d557f2f5b2c411a3fe027dc9ad
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-23 15:42:00 +0200

    deletion

--- src/main/java/com/docker/compose/cart/persistance/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docker.compose.cart.persistance.repository;
-
-import com.docker.compose.cart.persistance.entity.Cart;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends MongoRepository<Cart, String> {
-}
