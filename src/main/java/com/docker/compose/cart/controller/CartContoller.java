810c8ff24cceff34427541ef8f2e996ce88f4e6d
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-04-23 12:05:32 +0200

    Email Sender

--- src/main/java/com/docker/compose/cart/controller/CartContoller.java
+++ src/main/java/com/docker/compose/cart/controller/CartContoller.java
@@ -1,7 +1,9 @@
 package com.docker.compose.cart.controller;
 
+import com.docker.compose.cart.email.service.EmailService;
 import com.docker.compose.cart.service.CartService;
 import com.docker.compose.cart.persistance.entity.Cart;
+import com.docker.compose.user.persistance.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ public class CartContoller {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping("/cart")
     public ResponseEntity<List<Cart>> gerAllCart() {
         return ResponseEntity.ok().body(cartService.getAllCart());
@@ -29,6 +34,9 @@ public class CartContoller {
 
     @PostMapping("/cart")
     public ResponseEntity < Cart > createCart(@RequestBody Cart cart) {
+        User recipient = cart.getUser();
+        emailService.sendEmail(recipient.getEmail(), "Your order no." + cart.getId(),
+                "Your order was successful. You ordered:" + cart.getProducts());
         return ResponseEntity.ok().body(this.cartService.createCart(cart));
     }
 
@@ -49,4 +57,5 @@ public class CartContoller {
         this.cartService.deleteCollection();
         return HttpStatus.OK;
     }
+
 }
